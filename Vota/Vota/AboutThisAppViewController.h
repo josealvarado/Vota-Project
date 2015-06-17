efcc0d9be6b8e3a1ee1a9c36ca51df486c457fca
josealvarado <josealvarado@Joses-MacBook-Pro-2.local>
2015-06-17 08:30:57 -0700

    Fixed iPad Constraint

---
+++ Vota/Vota/AboutThisAppViewController.h
@@ -0,0 +1,14 @@
+//
+//  AboutThisAppViewController.h
+//  Vota
+//
+//  Created by Jose Alvarado on 6/17/15.
+//  Copyright (c) 2015 ___Jose-Alvarado___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutThisAppViewController : UIViewController
+- (IBAction)backButtonPressed:(id)sender;
+
+@end
