98ebd22cdd6bfd4e78fecc4489b50a7c12403cbb
josealvarado <josealvarado@Joses-MacBook-Pro.local>
2015-09-21 23:07:49 -0700

    Small Changes

--- Vota/Vota/GetFriendsToVoteViewController.m
+++ Vota/Vota/GetFriendsToVoteViewController.m
@@ -39,16 +39,27 @@
 */
 
 - (IBAction)shareVotaButtoPressed:(id)sender {
-    
+        
     NSString *text = @"Have you registered to vote? Have you voted? Do both with the VOTA app! VOTAapp.com";
     NSURL *url = [NSURL URLWithString:@"www.votaapp.com"];
+//    
+//    UIActivityViewController *controller =
+//    [[UIActivityViewController alloc]
+//     initWithActivityItems:@[text, url]
+//     applicationActivities:nil];
     
-    UIActivityViewController *controller =
-    [[UIActivityViewController alloc]
-     initWithActivityItems:@[text, url]
-     applicationActivities:nil];
+    UIActivityViewController *controller = [[UIActivityViewController alloc] initWithActivityItems:@[text, url] applicationActivities:nil];
     
-    [self presentViewController:controller animated:YES completion:nil];
+    //if iPhone
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+        [self presentViewController:controller animated:YES completion:nil];
+    }
+    //if iPad
+    else {
+        // Change Rect to position Popover
+        UIPopoverController *popup = [[UIPopoverController alloc] initWithContentViewController:controller];
+        [popup presentPopoverFromRect:CGRectMake(self.view.frame.size.width/2, self.view.frame.size.height/4, 0, 0)inView:self.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+    }
 }
 
 - (IBAction)askRegisterButtonPressed:(id)sender {
